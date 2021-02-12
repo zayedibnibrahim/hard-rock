091d7c5ef903a773d55d94c0ec3c6a32dbc3f98c
zayedibnibrahim <zayed.ibn@gmail.com>
2021-02-13 00:43:58 +0600

    Music On!

--- app.js
+++ app.js
@@ -6,28 +6,55 @@ const searchBtn = () => {
 const fetchSongInfo = async songName => {
     const apiBase = 'https://api.lyrics.ovh/suggest/';
     const url = `${apiBase}${songName}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    showSongResult(data);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        showSongResult(data);
+    } catch (error) {
+        errorMessage();
+    }
 }
-
+//error handler
+const errorMessage = () => {
+    const errorText = document.getElementById("error-message");
+    errorText.style.display = "block";
+    errorText.innerText = "Sorry, song Not Found";
+    console.log(error);
+}
 const showSongResult = result => {
+    const errorText = document.getElementById("error-message");
+    errorText.style.display = "none";
     const allSong = result.data;
     const songContainer = document.getElementById("songs-container");
     songContainer.innerHTML = '';
     allSong.forEach(singleSong => {
         const songTitle = singleSong.title;
-        const songAlbum = singleSong.artist.name;
+        const songArtist = singleSong.artist.name;
+        const songPreview = singleSong.preview;
         const innerDiv = document.createElement("div")
         innerDiv.className = "single-result row align-items-center my-3 p-3"
         const songContent = `<div class="col-md-9">
         <h3 class="lyrics-name">${songTitle}</h3>
-        <p class="author lead">Album by <span>${songAlbum}</span></p>
+        <p class="author lead">Album by <span>${songArtist}</span></p>
+        <audio controls>
+            <source src="${songPreview}">
+        </audio>
     </div>
     <div class="col-md-3 text-md-right text-center">
-        <button class="btn btn-success">Get Lyrics</button>
+        <button onclick="fetchLyrics('${songArtist}', '${songTitle}')" class="btn btn-success">Get Lyrics</button>
     </div>`
-    innerDiv.innerHTML = songContent;
-    songContainer.appendChild(innerDiv);
+        innerDiv.innerHTML = songContent;
+        songContainer.appendChild(innerDiv);
     });
+}
+const fetchLyrics = async (sArtist, sTitle) => {
+    const lUrl = `https://api.lyrics.ovh/v1/${sArtist}/${sTitle}`;
+    const lRes = await fetch(lUrl);
+    const lData = await lRes.json();
+    showLyrics(lData);
+}
+const showLyrics = sLyrics => {
+    const lyricsContainer = document.getElementById("lyrics-container");
+    const getLyrics = sLyrics.lyrics;
+    lyricsContainer.innerText = getLyrics;
 }
